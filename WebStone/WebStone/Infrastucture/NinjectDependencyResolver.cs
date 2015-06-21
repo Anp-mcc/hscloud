6fd09e10787a660483443486f06c2e1cb642b1a1
Anp-mcc <andrey.popov@mcc-tomsk.de>
2015-06-21 19:02:28 +0600

    Add NinjectDependencyResolver and simple HomeController for test

---
+++ WebStone/WebStone/Infrastucture/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Web.Services.Protocols;
+using Ninject;
+using WebStone.Controllers;
+
+namespace WebStone.Infrastucture
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly StandardKernel _kernel;
+
+        public NinjectDependencyResolver()
+        {
+            _kernel = new StandardKernel();
+            AddBinding();
+        }
+
+        private void AddBinding()
+        {
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return _kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _kernel.GetAll(serviceType);
+        }
+    }
+}
