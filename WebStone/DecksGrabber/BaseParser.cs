aba42b1a44a0ab22d4f62992d26271204e8af141
Igor Sokolov <igorgrru@gmail.com>
2015-08-27 00:07:38 +0600

    Fixed crash in decks grabber.

--- WebStone/DecksGrabber/BaseParser.cs
+++ WebStone/DecksGrabber/BaseParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
@@ -31,67 +32,73 @@ namespace DecksGrabber
                 // Repeat while at least for one class we still see new for us decks
                 while (allDecksAreLoaded.Where(x => !x.Value).Count() > 0)
                 {
-                    List<Task<string>> downloadingDecksTasks = new List<Task<string>>();
-                    List<KeyValuePair<DeckInfo, Task<string>>> downloadingDeckTasks = new List<KeyValuePair<DeckInfo, Task<string>>>();
-
-                    // Start downloading first decks page for all classes
-                    foreach (var className in _classNames)
+                    try
                     {
-                        if (!allDecksAreLoaded[className])
+                        List<Task<string>> downloadingDecksTasks = new List<Task<string>>();
+                        List<KeyValuePair<DeckInfo, Task<string>>> downloadingDeckTasks = new List<KeyValuePair<DeckInfo, Task<string>>>();
+
+                        // Start downloading first decks page for all classes
+                        foreach (var className in _classNames)
                         {
-                            string url = GetDecksUrl(className, pageIndex);
-                            if (!string.IsNullOrEmpty(url))
+                            if (!allDecksAreLoaded[className])
                             {
-                                Task<string> downloadTask = DownloadPage(url);
-                                downloadingDecksTasks.Add(downloadTask);
+                                string url = GetDecksUrl(className, pageIndex);
+                                if (!string.IsNullOrEmpty(url))
+                                {
+                                    Task<string> downloadTask = DownloadPage(url);
+                                    downloadingDecksTasks.Add(downloadTask);
+                                }
                             }
                         }
-                    }
 
-                    // Wait for each response and parse this
-                    Task.WaitAll(downloadingDecksTasks.ToArray());
-                    foreach (var downloadingDecksTask in downloadingDecksTasks)
-                    {
-                        string decksPageContent = downloadingDecksTask.Result;
-
-                        // Parse downloaded decks page
-                        var decks = ParseDecksPage(decksPageContent);
-
-                        // Start downloading page for each deck
-                        foreach (var deckInfo in decks)
+                        // Wait for each response and parse this
+                        Task.WaitAll(downloadingDecksTasks.ToArray());
+                        foreach (var downloadingDecksTask in downloadingDecksTasks)
                         {
-                            if (!string.IsNullOrEmpty(deckInfo.DeckUrl))
+                            string decksPageContent = downloadingDecksTask.Result;
+
+                            // Parse downloaded decks page
+                            var decks = ParseDecksPage(decksPageContent);
+
+                            // Start downloading page for each deck
+                            foreach (var deckInfo in decks)
                             {
-                                Task<string> downloadTask = DownloadPage(deckInfo.DeckUrl);
-                                downloadingDeckTasks.Add(new KeyValuePair<DeckInfo, Task<string>>(deckInfo, downloadTask));
+                                if (!string.IsNullOrEmpty(deckInfo.DeckUrl))
+                                {
+                                    Task<string> downloadTask = DownloadPage(deckInfo.DeckUrl);
+                                    downloadingDeckTasks.Add(new KeyValuePair<DeckInfo, Task<string>>(deckInfo, downloadTask));
+                                }
                             }
                         }
-                    }
 
-                    // Wait for each response and parse this
-                    bool nothingUpdated = true;
-                    Task.WaitAll(downloadingDeckTasks.Select(x => x.Value).ToArray());
-                    foreach (var downloadingDeckTask in downloadingDeckTasks)
-                    {
-                        string deckPageContent = downloadingDeckTask.Value.Result;
-
-                        // Parse downloaded deck page
-                        DeckInfo deckInfoDetailed = ParseDeckPage(deckPageContent);
-
-                        downloadingDeckTask.Key.Cards = deckInfoDetailed.Cards;
-                        downloadingDeckTask.Key.Description = deckInfoDetailed.Description;
-
-                        if (ValidateDeckInfo(downloadingDeckTask.Key) && DeckParsedCallback != null)
+                        // Wait for each response and parse this
+                        bool nothingUpdated = true;
+                        Task.WaitAll(downloadingDeckTasks.Select(x => x.Value).ToArray());
+                        foreach (var downloadingDeckTask in downloadingDeckTasks)
                         {
-                            bool somethingUpdated = DeckParsedCallback(downloadingDeckTask.Key);
-                            if (somethingUpdated)
-                                nothingUpdated = false;
+                            string deckPageContent = downloadingDeckTask.Value.Result;
+
+                            // Parse downloaded deck page
+                            DeckInfo deckInfoDetailed = ParseDeckPage(deckPageContent);
+
+                            downloadingDeckTask.Key.Cards = deckInfoDetailed.Cards;
+                            downloadingDeckTask.Key.Description = deckInfoDetailed.Description;
+
+                            if (ValidateDeckInfo(downloadingDeckTask.Key) && DeckParsedCallback != null)
+                            {
+                                bool somethingUpdated = DeckParsedCallback(downloadingDeckTask.Key);
+                                if (somethingUpdated)
+                                    nothingUpdated = false;
+                            }
+
+                            allDecksAreLoaded[downloadingDeckTask.Key.Class] = nothingUpdated;
                         }
 
-                        allDecksAreLoaded[downloadingDeckTask.Key.Class] = nothingUpdated;
+                        ++pageIndex;
                     }
-
-                    ++pageIndex;
+                    catch (Exception)
+                    {
+                    }
                 }
 
                 Thread.Sleep(delaySeconds * 1000);
